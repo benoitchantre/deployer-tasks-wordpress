70428585d1e134ebfd9e85ea83cdabe9a4df515e
Benoît Chantre <mail@benoitchantre.com>
2021-11-24 16:51:55 +0100

    Don't fail when OPcache is not installed

--- src/opcache.php
+++ src/opcache.php
@@ -7,6 +7,12 @@ task('opcache:clear', function() {
     within(
         '{{release_path}}',
         function(): void {
+            // Exit early OPcache is not available.
+            if (! function_exists('opcache_reset')) {
+                write('<comment>Aborted: OPcache is not available on the host.</comment>');
+                return;
+            }
+
             // Exit early when WP-CLI Clear OPcache is not installed.
             if (! test('wp cli has-command opcache')) {
                 write('<comment>Aborted: WP-CLI Clear OPcache is not installed.</comment>');
