7aae9308596b0b773832cc60bd46db5cd907c8b2
Benoît Chantre <mail@benoitchantre.com>
2021-12-14 21:03:48 +0100

    Allow to skip opcache:clear from the host configuration

--- src/opcache.php
+++ src/opcache.php
@@ -7,6 +7,12 @@ task('opcache:clear', function() {
     within(
         '{{release_path}}',
         function(): void {
+            // Exit early when `opcache` attribute is set to false for the host.
+            if (! get('opcache', true)) {
+                writeln('<comment>Aborted: OPcache is marked as not available for this host.</comment>');
+                return;
+            }
+
             // Exit early when WP-CLI Clear OPcache is not installed.
             if (! test('wp cli has-command opcache')) {
                 writeln('<comment>Aborted: WP-CLI Clear OPcache is not installed.</comment>');
