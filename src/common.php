89527ca4ac56e2754825d74e748b12f5639cbfc9
Benoît Chantre <mail@benoitchantre.com>
2022-11-01 22:46:29 +0100

    Fix which doesn't exist in Deployer 6

--- src/common.php
+++ src/common.php
@@ -4,5 +4,11 @@ namespace Deployer;
 
 // Return path to the `wp` bin.
 set('bin/wp', function () {
+
+    // Backward compatibility for Deployer 6
+    if (function_exists('locateBinaryPath')) {
+        return locateBinaryPath('wp');
+    }
+
     return which('wp');
 });
